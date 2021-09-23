e7a0baf76264fb9f06534297d3160f8452ffb01b
Jadmir <jadmir1111@gmail.com>
2021-09-22 22:29:40 -0500

    actualizado

--- src/modules/daybook/store/journal/actions.js
+++ src/modules/daybook/store/journal/actions.js
@@ -4,15 +4,30 @@ import juornalApi from '@/api/journalApi';
 
 // }
 
-export const loadEntries = async (/*{ commit }*/) => {
+export const loadEntries = async ( { commit }) => {
 
     const { data } = await juornalApi.get('/entries.json')
-    console.log(data)
+    const entries = []
+    for( let id of Object.keys( data )) {
+        entries.push({
+            id,
+            ...data[id]
+        })
+    }
+
+    commit('setEntries', entries )
 
 }
 
-export const updateEntry = async (/*{ commit }*/) => {
+export const updateEntry = async ({ commit }, entry) => {
 
+    const { data, picture, text } = entry
+    const dataToSave = { data, picture, text }
+
+    const resp = await juornalApi.put( `/entries/${ entry.id }.json`, dataToSave )
+    console.log(resp)
+
+    commit('updateEntry', { ...entry })
 }
 
 
--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -40,13 +40,14 @@
 
     <Fab 
         icon="fa-save"
+        @on:click="saveEntry"
     />
 
 </template>
 
 <script>
 import { defineAsyncComponent } from 'vue'
-import { mapGetters } from 'vuex' // computed!!!
+import { mapGetters, mapActions } from 'vuex' // computed!!!
 
 import getDayMonthYear from '../helpers/getDayMonthYear'
 
@@ -84,11 +85,18 @@ export default {
     },
 
     methods: {
+        ...mapActions('journal', ['updateEntry']),
+
         loadEntry() {
             const entry = this.getEntryById( this.id )
             if ( !entry ) return this.$router.push({ name: 'no-entry' })
 
             this.entry = entry
+        },
+        async saveEntry(){
+            console.log('Guardando Entrada')
+
+            this.updateEntry(this.entry)
         }
     },
 
